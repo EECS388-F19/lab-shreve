a64bf05e60d61f6a569d6bfb7f35cae589359630
Jacob Evan Shreve <jacob@shreve.io>
2019-09-06 16:53:01 -0400

    Add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import random
+
+if __name__ == '__main__':
+    num1 = random.randint(0,100)
+    num2 = random.randint(0,100)
+    sum = num1 + num2
+    avg = sum / 2.0
+    print("Jacob")
+    print(num1)
+    print(num2)
+    print(f'Sum = {sum}')
+    print(f'Average = {avg}')
